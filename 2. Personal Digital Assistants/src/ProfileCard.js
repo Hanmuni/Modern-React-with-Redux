e7f36c87ead79ebee7c81841274d834db2b4f8b6
Hong-Hanh Chu <67581292+Hanmuni@users.noreply.github.com>
2026-01-30 15:24:07 +0100

    add personal digital assistans project

---
+++ 2. Personal Digital Assistants/src/ProfileCard.js
@@ -0,0 +1,18 @@
+export default function ProfileCard({ title, handle, image, description }) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="assistant logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">Title is: {title}</p>
+          <p className="subtitle is-7">Handle is: {handle}</p>
+          <p className="content">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
