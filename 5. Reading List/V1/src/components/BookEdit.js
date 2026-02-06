8ceae10cf8386514d5de84ba3fb21db99d1fa61c
Hong-Hanh Chu <67581292+Hanmuni@users.noreply.github.com>
2026-02-07 00:43:59 +0100

    add reading list project with context hook

--- 5. Reading List/V1/src/components/BookEdit.js
+++ 5. Reading List/V1/src/components/BookEdit.js
@@ -15,7 +15,6 @@ function BookEdit({ book, onSubmit }) {
   return (
     <form className="book-edit" onSubmit={handleSubmit}>
       <label>Title</label>
-
       <input className="input" value={title} onChange={handleChange} />
       <button className="button is-primary"> Save</button>
     </form>
